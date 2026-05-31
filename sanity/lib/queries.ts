9759e35e7f046f6e0b482fd1659204c5beb3494c
Divya <divyadarsheel.dds@gmail.com>
2026-05-31 18:53:58 +0545

    chore: added the getClubBySlug query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -19,3 +19,28 @@ export const homepageQuery = groq`*[_id == "homepage"][0] {
 export async function getHomepageData() {
   return await client.fetch(homepageQuery)
 }
+
+// 🚨 NEW CONCEPT: GROQ PARAMETERS ($slug)
+export const clubBySlugQuery = groq`*[_type == "club" && slug.current == $slug][0] {
+  name,
+  slug,
+  description,
+  logo,
+  heroImage,
+  "clubHeads": clubHeads[]-> {
+    name,
+    photo,
+    grade
+  },
+  "viceHeads": viceHeads[]-> {
+    name,
+    photo,
+    grade
+  },
+  achievements
+}`
+
+export async function getClubBySlug(slug: string) {
+  // We pass the slug variable safely into the query
+  return await client.fetch(clubBySlugQuery, { slug })
+}
