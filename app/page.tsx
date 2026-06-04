82ae79e020dcaca72517d3f39489e74090920afb
Divya <divyadarsheel.dds@gmail.com>
2026-06-04 13:40:49 +0545

    chore: edited the page to match the structure for about section

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function HomePage() {
 
   return (
     <main className="min-h-screen bg-bg">
-      <HomeScrollExperience />
+      <HomeScrollExperience data={data} />
     </main>
   );
 }
